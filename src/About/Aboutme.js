9deda52e5c63d99c7cac42297acf8cd54705d7f3
Nagabalan-cns <117824296+Nagabalan-cns@users.noreply.github.com>
2023-01-31 15:57:11 +0530

    typewriter Effect

--- src/About/Aboutme.js
+++ src/About/Aboutme.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import {Link} from "react-scroll"
+import TypeWriter from "typewriter-effect"
 import"./aboutme.css"
 function Aboutme() {
   return (
     <div className='topcontent'>
         <div className='topcontent_container'>
-       <h1>Mr.K.Nagabalan </h1> 
-       <p>A Professional Web Developer</p>
+       <h1>Hello Guys!</h1> 
+       <h2>
+        <TypeWriter
+        options={{
+          autoStart: true,
+          loop:true,
+          delay:50,
+          strings:[
+            'I am Mr.k.Nagabalan',
+            'I am a Web Developer',
+            "I am a React Developer"
+          ]
+        }}
+        />
+       </h2>
        <a href="https://github.com/Nagabalan-cn" target="blank">
      <button className='Topcontent_download' >Github Link</button>
      </a>
